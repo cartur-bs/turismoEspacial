cd89ed109c41f96abe519f021517c69684938bcb
Carlos Artur <cartur758@gmail.com>
2023-06-13 23:31:52 -0300

    media-query-time

--- src/Pages/Time.js
+++ src/Pages/Time.js
@@ -1,17 +1,18 @@
 import './Time.css'
 import { useState } from 'react';
+import fotoCapitao from '../assets/crew/image-douglas-hurley.png';
+import fotoEngenheira from '../assets/crew/image-anousheh-ansari.png';
+import fotoPiloto from '../assets/crew/image-victor-glover.png';
+import fotoEspecialista from '../assets/crew/image-mark-shuttleworth.png';
+
 
 const Time = () => {
+    const [mudaFoto, setMudaFoto] = useState(fotoCapitao);
+
 
     const [papelExercido, setPapelExercido] = useState('Capitão');
     const [pessoaResponsavel, setPessoaResponsavel] = useState('Douglas Hurley');
     const [textoApresentacao, setTextoApresentacao] = useState('Douglas Gerald Hurley é um engenheiro Norte-Americano, ex piloto da marinha e ex astronauta da NASA. Ele foi ao espaço pela terceira vez como comandante da Crew Dragon Demo-2.');
-
-    const fotoIntegrante =  document.getElementById('fotoDoIntegrante');
-    const botaoPiloto = document.getElementById('btnPiloto');
-    const botaoEngenheira = document.getElementById('btnEngenheira');
-    const botaoEspecialista = document.getElementById('btnEspecialista');
-    const botaoCapitao = document.getElementById('btnCapitao');
 
 
     const engenheiraPerfil = () => {
@@ -19,48 +20,26 @@ const Time = () => {
         setPessoaResponsavel('Anousheh ansari');
         setTextoApresentacao('Anousheh Ansari é uma engenheira Irã-americana e co-fundadora dos Sistemas Prodea. Anousheh foi a quarta turista no espaço, sendo a primeira mulher a visitar a ISS e a primeira iraniana no espaço.');
 
-        fotoIntegrante.classList.add('fotoEngenheira');
-        fotoIntegrante.classList.remove('fotoCapitao');
-        fotoIntegrante.classList.remove('fotoPiloto');
-        fotoIntegrante.classList.remove('fotoEspecialista');
-
-        botaoCapitao.classList.remove('btnAberto');
-        botaoEngenheira.classList.add('btnAberto');
-        botaoEspecialista.classList.remove('btnAberto');
-        botaoPiloto.classList.remove('btnAberto');
+        setMudaFoto(fotoEngenheira)
 
     }
 
-    const comandantePerfil = () => {
+    const capitaoPerfil = () => {
         setPapelExercido('Comandante');
         setPessoaResponsavel('Douglas Hurley');
         setTextoApresentacao('Douglas Gerald Hurley é um engenheiro Norte-Americano, ex piloto da marinha e ex astronauta da NASA. Ele foi ao espaço pela terceira vez como comandante da Crew Dragon Demo-2.');
 
-        fotoIntegrante.classList.add('fotoCapitao');
-        fotoIntegrante.classList.remove('fotoEngenheira');
-        fotoIntegrante.classList.remove('fotoEspecialista');
-        fotoIntegrante.classList.remove('fotoPiloto');
+        setMudaFoto(fotoCapitao)
 
-        botaoCapitao.classList.add('btnAberto');
-        botaoEngenheira.classList.remove('btnAberto');
-        botaoEspecialista.classList.remove('btnAberto');
-        botaoPiloto.classList.remove('btnAberto');
     }
 
     const pilotoPerfil = () => {
         setPapelExercido('Piloto');
         setPessoaResponsavel('Victor Glover');
         setTextoApresentacao('Piloto no primeiro voo operacional da SpaceX Crew Dragon à ISS, Glover é comandante na marinha dos EUA onde pilota F/A-18. Ele foi membro do time na experidção de 64 e serviu  como engenheiro de voo em estados de operação.')
-  
-        fotoIntegrante.classList.remove('fotoCapitao');
-        fotoIntegrante.classList.remove('fotoEngenheira');
-        fotoIntegrante.classList.remove('fotoEspecialista');
-        fotoIntegrante.classList.add('fotoPiloto');
 
-        botaoCapitao.classList.remove('btnAberto');
-        botaoEngenheira.classList.remove('btnAberto');
-        botaoEspecialista.classList.remove('btnAberto');
-        botaoPiloto.classList.add('btnAberto');
+        setMudaFoto(fotoPiloto)
+
     }
 
     const especialistaPerfil = () => {
@@ -68,30 +47,24 @@ const Time = () => {
         setPessoaResponsavel('Mark Shuttleworth');
         setTextoApresentacao('Mark Richard Shuttleworth é fundador e CEO da Cannonical, a empresa por trás do sistema operacional Ubuntu baseado em Linux. Shuttleworth se tornou o primeiro Sul-Africano a viajar ao espaço como turista.');
 
-        fotoIntegrante.classList.remove('fotoCapitao');
-        fotoIntegrante.classList.remove('fotoEngenheira');
-        fotoIntegrante.classList.remove('fotoPiloto');
-        fotoIntegrante.classList.add('fotoEspecialista');
+        setMudaFoto(fotoEspecialista)
 
-        botaoCapitao.classList.remove('btnAberto');
-        botaoEngenheira.classList.remove('btnAberto');
-        botaoEspecialista.classList.add('btnAberto');
-        botaoPiloto.classList.remove('btnAberto');
     }
 
     return (
         <div className='timePage'>
-            <h2><span>02</span> Conheça seu time</h2>
-            <div>
-                <div className='fotoCapitao' id='fotoDoIntegrante'>
-                    <img src="../assets/crew/image-anousheh-ansari.png" alt="" />
+            <div className="tituloSessao">
+                <h2><span>02</span> Conheça seu time</h2>
+            </div>
+            <div className='timeMain'>
+                <div className='fotoDoIntegrante'>
+                    <img src={mudaFoto} alt="Foto do capitão Douglas Hurley" />
                 </div>
-
                 <div>
                     <h3 id='papelExercido'>{papelExercido}</h3>
                     <h2 id='pessoaResponsavel'>{pessoaResponsavel}</h2>
                     <p id='textoApresentacao'>{textoApresentacao} </p>
-                    <button className='btnMudaTime' id='btnCapitao' onClick={comandantePerfil}></button>
+                    <button className='btnMudaTime' id='btnCapitao' onClick={capitaoPerfil}></button>
                     <button className='btnMudaTime' id='btnPiloto' onClick={pilotoPerfil}></button>
                     <button className='btnMudaTime' id='btnEngenheira' onClick={engenheiraPerfil}></button>
                     <button className='btnMudaTime' id='btnEspecialista' onClick={especialistaPerfil}></button>
